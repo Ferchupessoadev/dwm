7a9b1398a586b6cc70a9886cc46a0063754800b5
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-10-07 20:27:57 -0300

    added commands in other file

---
+++ commands.h
@@ -0,0 +1,53 @@
+// commands.h
+#include <stdlib.h>
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+/* commands */
+/* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0";
+
+static const char *dmenucmd[] = {
+    "dmenu_run",
+    NULL,
+};
+
+static const char *launcher[] = {
+    "dmenu_run_desktop",
+    "-c",
+    NULL,
+};
+
+static const char *termcmd[] = {
+    "st",
+    NULL,
+};
+
+static const char *fileexplorer[] = {
+    "file_manager.sh",
+    NULL,
+};
+
+static const char *chromium[] = {
+    "chromium",
+    NULL,
+};
+
+static const char *screenshot[] = {
+    "flameshot", "screen", "--path", "$HOME/Imágenes/screenshot/", NULL,
+};
+
+static const char *volumeup[] = {
+    "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%", NULL,
+};
+
+static const char *volumedown[] = {
+    "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-10%", NULL,
+};
+
+static const char *volumemute[] = {
+    "pactl", "set-sink-volume", "@DEFAULT_SINK@", "toggle", NULL,
+};
+
+#endif // COMMANDS_H
--- config.h
+++ config.h
@@ -30,10 +30,10 @@ static const char col_cyan[] = "#005577";
 
 // themes
 struct Theme {
-  char *inactive;
-  char *active;
-  char *bg;
-  char *focus;
+  const char *inactive;
+  const char *active;
+  const char *bg;
+  const char *focus;
 };
 
 static const struct Theme material = {
@@ -130,7 +130,8 @@ static const char *colors[][3] = {
     {tomorrow_night.active, tomorrow_night.focus, tomorrow_night.focus},
 };
 
-static const char *tags[] = {"1", "2", "3", "4"}; // tags
+static const char *tags[] = {"1", "2", "3", "4", "5",
+                             "6", "7", "8", "9"}; // tags
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -177,53 +178,8 @@ static const Layout layouts[] = {
 
 #define STATUSBAR "dwmblocks"
 
-/* commands */
-/* component of dmenucmd, manipulated in spawn() */
-static char dmenumon[2] = "0";
-
-static const char *dmenucmd[] = {
-    "dmenu_run",
-    NULL,
-};
-
-static const char *launcher[] = {
-    "dmenu_run_desktop",
-    "-c",
-    NULL,
-};
-
-static const char *termcmd[] = {
-    "st",
-    NULL,
-};
-
-static const char *fileexplorer[] = {
-    "file_manager.sh",
-    NULL,
-};
-
-static const char *chromium[] = {
-    "chromium",
-    NULL,
-};
-
-static const char *screenshot[] = {
-    "flameshot", "screen", "--path", "$HOME/Imágenes/screenshot/", NULL,
-};
-
-static const char *volumeup[] = {
-    "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%", NULL,
-};
-
-static const char *volumedown[] = {
-    "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-10%", NULL,
-};
-
-static const char *volumemute[] = {
-    "pactl", "set-sink-volume", "@DEFAULT_SINK@", "toggle", NULL,
-};
-
 #include "./exitdwm.c"
+#include "commands.h"
 #include <X11/XF86keysym.h>
 static const Key keys[] = {
     /* modifier                     key        function        argument */
@@ -249,7 +205,7 @@ static const Key keys[] = {
     {MODKEY, XK_p, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_Return, zoom, {0}},
+    {MODKEY | ShiftMask, XK_Return, zoom, {0}},
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY | ShiftMask, XK_q, killclient, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
